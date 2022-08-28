54b9f27ab195d009559b9139a32777ad7f23799f
yunusorbeyi <111960442+ventre6@users.noreply.github.com>
2022-08-28 12:53:03 +0300

    Updatee

--- server-data/baslat.bat
+++ server-data/baslat.bat
@@ -1,4 +1,4 @@
-cd /d C:\Users\erold\Desktop\nopixeltemel\server-data
-start C:\Users\erold\Desktop\nopixeltemel\server\FXServer.exe +exec server.cfg +set sv_enforceGameBuild 2372 +set onesync on +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +set onesync_population true
+cd /d C:\Users\erold\Documents\GitHub\Nopixel-Temel\server-data
+start C:\Users\erold\Documents\GitHub\Nopixel-Temel\server\FXServer.exe +exec server.cfg +set sv_enforceGameBuild 2372 +set onesync on +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +set onesync_population true
 
 exit
